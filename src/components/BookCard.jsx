7931c0ea9209dc39fadb30cf5ac47468e0e4286f
Kinga Jakubowska <laozix@wp.pl>
2024-03-28 03:47:02 +0100

    basic components start up
    start up for components

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,3 @@
+export default function BookCard() {
+    return <h1>Here is more about the book</h1>
+}
---
+++ src/components/SearchResults.jsx
@@ -0,0 +1,3 @@
+export default function SearchResults() {
+    return <h1>Click to find Bond</h1>
+}
