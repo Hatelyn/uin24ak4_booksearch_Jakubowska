a3063c83caf05dbb5daff24fb9775236aab08a47
Hatelyn <91886259+Hatelyn@users.noreply.github.com>
2024-04-02 01:41:38 +0200

    amazon id fix + mui
    mui for rating, amazon id fix, multiple authors showing correctly

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,3 +1,4 @@
+import { Rating } from "@mui/material"
 import { useEffect, useState } from "react"
 
 export default function Search({ books, setQuery, query, prevSearch, setPrevSearch }) {
@@ -37,10 +38,17 @@ export default function Search({ books, setQuery, query, prevSearch, setPrevSear
                     <article key={product.key}>
                         <h4>{product.title}</h4>
                         <img src={`https://covers.openlibrary.org/b/id/${product.cover_i}-M.jpg`} alt="book picture" />
-                        <p>Author/s: {product.author_name}</p>
+                        <p>Author/s: {product.author_name ? product.author_name.join(", ") : "Unknown author"}</p> {/* Fortsette å hadde undefinied error, så kom til at det må være noen bok som ikke har en forfatter */}
                         <p>First publish: {product.first_publish_year}</p>
-                        <p>Average rating: {product.ratings_average ? product.ratings_average : "No data"}</p>
-                        <button><a href={`https://www.amazon.com/s?k=${product.isbn ? product.isbn[0] : null}`} className={product.isbn ? "inStock" : "empty"}>{product.isbn ? "Buy on Amazon" : "Out of stock"}</a></button>
+                        {product.ratings_average > 0 ? <p>Rating: {product.ratings_average.toFixed(1)}<br />
+                            <Rating name="read-only" value={product.ratings_average} precision={0.5} readOnly className="rating" /></p>
+                            :
+                            <p>No rating given <br />
+                                <Rating name="no-value" value={null} readOnly className="rating" /></p>}
+                        {product.id_amazon?.[0]?.length > 0 ?
+                            <button><a href={`https://www.amazon.com/s?k=${product.id_amazon[0]}`}>Buy on Amazon</a></button>
+                            :
+                            <button disabled>Out of stock</button>}
                     </article>
                 )}
             </div>
